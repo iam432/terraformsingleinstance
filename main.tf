308a3e73abe7a9caeee2556fcefc657e7d130075
iam432 <60140578+iam432@users.noreply.github.com>
2020-06-30 22:14:34 +0200

    Update main.tf
    ok

--- main.tf
+++ main.tf
@@ -91,29 +91,29 @@ resource "aws_security_group" "allow_all" {
     }
 }
 
-data "aws_ami" "my_ami" {
-     most_recent      = true
-     #name_regex       = "^mavrick"
-     owners           = ["472428388205"]
-}
+#data "aws_ami" "my_ami" {
+#     most_recent      = true
+#     #name_regex       = "^mavrick"
+#     owners           = ["472428388205"]
+#}
 
 
-# resource "aws_instance" "web-1" {
-#     ami = "${data.aws_ami.my_ami.id}"
-#     #ami = "ami-0d857ff0f5fc4e03b"
-#     availability_zone = "us-east-1a"
-#     instance_type = "t2.micro"
-#     key_name = "LaptopKey"
-#     subnet_id = "${aws_subnet.subnet1-public.id}"
-#     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
-#     associate_public_ip_address = true	
-#     tags = {
-#         Name = "Server-1"
-#         Env = "Prod"
-#         Owner = "Sree"
-# 	CostCenter = "ABCD"
-#     }
-# }
+ resource "aws_instance" "web-1" {
+     ami = "${var.customami}"
+     #ami = "ami-0d857ff0f5fc4e03b"
+     availability_zone = "us-east-1a"
+     instance_type = "t2.micro"
+     key_name = "Laptopkey"
+     subnet_id = "${aws_subnet.subnet1-public.id}"
+     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
+     associate_public_ip_address = true	
+     tags = {
+         Name = "Server-1"
+         Env = "Prod"
+         Owner = "kk"
+ 	CostCenter = "432"
+     }
+ }
 
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"
